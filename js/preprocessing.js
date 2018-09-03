a0eb444d08f2ba5624df87fce195c38bf112d6a7
Jojooo0o <jonasdorsch@web.de>
2018-09-03 18:00:29 +0200

    csv successful read.

--- js/preprocessing.js
+++ js/preprocessing.js
@@ -4,12 +4,13 @@ var dataPaths = ["../data/mass_shootings_2014.csv", "../data/mass_shootings_2015
 
 
 function parseCSV() {
-  /*d3.csv("../data/mass_shootings_2014.csv", function(data) {
-    console.log(data[0]);
-  });*/
-
-  Promise.all(d3.csv("../data/mass_shootings_2014.csv")).then(
-  function(data) {
-    console.log(data[0])
+  Promise.all([d3.csv(dataPaths[0]),
+              d3.csv(dataPaths[1]),
+              d3.csv(dataPaths[2]),
+              d3.csv(dataPaths[3])])
+  .then(function(data) {
+    for(var i = 0; i <= dataPaths.length; ++i)
+      console.log(data[i]);
+  }).catch(function(error) {
   });
 }
