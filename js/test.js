5ff2e6027e63e3f830f88818564986d9c9f83476
Jojooo0o <jonasdorsch@web.de>
2018-09-07 23:04:42 +0200

    Linegraph reworked.

--- js/test.js
+++ js/test.js
@@ -2,7 +2,7 @@ var dataPaths = ["../data/mass_shootings_2014.csv", "../data/mass_shootings_2015
                  "../data/mass_shootings_2016.csv", "../data/mass_shootings_2017.csv",
                  "../data/mass_shootings_2018.csv"];     // saves paths of data sets
 
-var lineColors = ["red", "blue", "green", "yellow", "orange"];
+var lineColors = ["red", "blue", "green", "yellow", "orange", "black", "black", "black", "black", "black"];
 
 
 function test() {
@@ -13,9 +13,9 @@ function test() {
   var parseAgain = d3.timeParse("%B %d");
 
   var margin = {top: 20, right: 50, bottom: 30, left: 50},
-    width = 1700 - margin.left - margin.right,
+    width = 1000 - margin.left - margin.right,
     // width = +svg.attr("width") - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+    height = 300 - margin.top - margin.bottom;
     // height = +svg.attr("height") - margin.top - margin.bottom,
 
   var svg = d3.select("svg")
@@ -28,13 +28,14 @@ function test() {
   var y = d3.scaleLog().range([height, 0]);
 
   var line = d3.line()
-    .x(function(d) { return x(d.date); })
+    .x(function(d) { return x(d.dateFake); })
     .y(function(d) { return y(d.killed + d.injured); });
 
   var div = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
+  var year_data;
 
   d3.queue()
     .defer(d3.csv, dataPaths[0])
@@ -43,12 +44,19 @@ function test() {
     .defer(d3.csv, dataPaths[3])
     .defer(d3.csv, dataPaths[4])
     .awaitAll(function (error, d) {
+      year_data = JSON.parse(JSON.stringify(d));
       for(var i = 0; i < d.length; ++i) {
         for(var j = 0; j < d[i].length; ++j) {
           // d[i][j].date = parseAgain(formatTime(parseTime(d[i][j]["Incident Date"])));
           d[i][j].date = parseTime(d[i][j]["Incident Date"]);
+          d[i][j].dateFake = parseAgain(formatTime(parseTime(d[i][j]["Incident Date"])));
           d[i][j].killed = +d[i][j]["# Killed"];
           d[i][j].injured = +d[i][j]["# Injured"];
+          year_data[i][j].date = parseTime(d[i][j]["Incident Date"]);
+          year_data[i][j].dateFake = parseAgain(formatTime(parseTime(d[i][j]["Incident Date"])));
+          year_data[i][j].killed = +d[i][j]["# Killed"];
+          year_data[i][j].injured = +d[i][j]["# Injured"];
+
         }
       }
       //console.log(d[0][0].date);
@@ -57,8 +65,15 @@ function test() {
       var max = 0;
       var min = 0;
 
+      console.log(year_data);
+
+      for(var i = 0; i < year_data.length; ++i) {
+        year_data[i] = calcYearOverview(year_data[i]);
+        d.push(year_data[i]);
+      }
+
       for(var i = 0; i < d.length; ++i){
-        d[i] = calcYearOverview(d[i]);
+        // d[i] = calcYearOverview(d[i]);
         max = d3.max(d[i], function(data) { return data.injured + data.killed});
         min = d3.min(d[i], function(data) { return data.injured + data.killed});
         if(max_injured_killed < max) {
@@ -69,22 +84,23 @@ function test() {
         }
       }
 
-      for(var i = 0; i < d.length; ++i) {
+      for(var i = 0; i < d.length/2; ++i) {
+      // for(var i = 0; i < 1; ++i) {
 
-        var last_date = d[i][0].date.toString();
-        var first_date = d[i][d[i].length-1].date.toString();
+        var last_date = d[i][0].dateFake.toString();
+        var first_date = d[i][d[i].length-1].dateFake.toString();
         // console.log(date);
-        d[i][0].date.setMonth(11,31);
-        d[i][d[i].length-1].date.setMonth(0,1);
+        d[i][0].dateFake.setMonth(11,31);
+        d[i][d[i].length-1].dateFake.setMonth(0,1);
         // d[i].unshift(d[i][0].date.setMonth(11,31));
         // for(var j = 0; j < d[0].length; ++j) {
         //   console.log("Day: " + d[0][j].date);
         // }
         // console.log("---------------------------");
         // d[i].push(d[i][d[i].length-1].date.setMonth(0,1));
-        x.domain(d3.extent(d[i], function(data) { return data.date; }));
-        d[i][0].date = new Date(last_date);
-        d[i][d[i].length-1].date = new Date(first_date);
+        x.domain(d3.extent(d[i], function(data) { return data.dateFake; }));
+        d[i][0].dateFake = new Date(last_date);
+        d[i][d[i].length-1].dateFake = new Date(first_date);
 
         // d[i].shift();
         // d[i].pop();
@@ -123,23 +139,53 @@ function test() {
             .text("Killed / Injured");
         }
 
+        console.log(line);
 
         svg.append("path")
-          .data([d[i]])
+          // .data([d[i]])
           .attr("class", "line")
-          .attr("d", line)
+          .attr("d", line(d[i]))
+          .attr("remember", i)
           .attr("stroke", lineColors[i])
           .attr("stroke-linejoin", "round")
           .attr("stroke-linecap", "round")
-          .attr("stroke-width", 1.5);
+          .attr("stroke-width", 1.5)
+          .transition()
+          .delay(2000)
+          .duration(2000)
+          // .attrTween("d", transformGraph(this));
+          .attrTween("d", function (de) {
+            console.log("remember me: " + this.getAttribute("remember"));
+            var prev = this.getAttribute("d");
+            console.log("prev: " + prev);
+            // var prev = d3.select(this).attr("d");
+            console.log(i);
+            var updated = line(d[parseInt(this.getAttribute("remember")) + 5]);
+            return d3.interpolatePath(prev, updated);
+          });
+          // // .call(transformGraph, line(d[i]), line(d[i+5]));
 
+        // transformGraph(line(d[i]), line(d[i+5]));
+
+        // d3.select('path')
+        //   .transition()
+        //   .duration(5000)
+        //   .attrTween('d', function (d) {
+        //     var previous = d3.select(this).attr('d');
+        //     var current = line(d[i+year_data.length]);
+        //     return d3.interpolatePath(previous, current);
+        //   });
+          // .transition()
+          //   .duration(500)
+          //   .on("start", transformGraph(line, raw_data[i]));
+            // .on("start", transformGraph(createLine(raw_data[i])));
 
         svg.selectAll("dot")
           .data(d[i])
           .enter().append("circle")
           .attr("r", 4.5)
           .attr("fill", lineColors[i])
-          .attr("cx", function(d) { return x(d.date); })
+          .attr("cx", function(d) { return x(d.dateFake); })
           .attr("cy", function(d) { return y(d.killed + d.injured); })
           .on("mouseover", function(d) {
             div.transition()
@@ -153,13 +199,6 @@ function test() {
             div.transition()
               .duration(500)
               .style("opactiy", 0);});
-          // .on("mouseover", function(d) { console.log(d.killed); console.log(d.date.getMonth()); showData(this, d.date);})
-          // .on("mouseout", function(){ hideData();});
-          // .append("title")
-          // .text("hello");
-          // .text(function(d) {return "Time: " + d.date +
-          // "\nKilled: " + d.killed
-          // "\nInjured: " + d.injured;});
       }
     });
 }
@@ -183,17 +222,3 @@ function calcYearOverview(data) {
   new_data.push(current_day);
   return new_data;
 }
-
-function showData(obj, d) {
- var coord = d3.mouse(obj);
- var infobox = d3.select(".infobox");
- // now we just position the infobox roughly where our mouse is
- infobox.style("left", (coord[0] + 100) + "px" );
- infobox.style("top", (coord[1] - 175) + "px");
- $(".infobox").html(d);
- $(".infobox").show();
- }
-
-function hideData() {
- $(".infobox").hide();
- }
